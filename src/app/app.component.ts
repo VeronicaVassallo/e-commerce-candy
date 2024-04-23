c0393a03d155ef10f2b239a2f856fbed15507229
VeronicaVassallo <vvassallo94@gmail.com>
2024-04-23 12:35:54 +0200

    method to select a product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  /*TO DO:
-  1) Al onClik di ogni card aggiungi un bordino alla card stessa */
   products = [
     {
+      _id: 1234,
       nameProduct: 'chewy rainbow',
       price: 10,
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -17,6 +16,7 @@ export class AppComponent {
       img: 'https://angolodolce.net/cdn/shop/products/chewy_rainbow_candy_gommosi_zuccherati_fini_4_1024x1024.jpg?v=1607166950',
     },
     {
+      _id: 5678,
       nameProduct: 'Candy Corn',
       price: 8,
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -25,6 +25,7 @@ export class AppComponent {
       img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Candy-Corn.jpg/1200px-Candy-Corn.jpg',
     },
     {
+      _id: 91011,
       nameProduct: 'jelly belly',
       price: 11,
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -33,6 +34,7 @@ export class AppComponent {
       img: 'https://www.ohnuts.com/noapp/showImage.cfm/zoom/_MG_5491.jpg',
     },
     {
+      _id: 1213,
       nameProduct: 'Candy Bear',
       price: 9,
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -41,6 +43,7 @@ export class AppComponent {
       img: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Oursons_g%C3%A9latine_march%C3%A9_Rouffignac.jpg',
     },
     {
+      _id: 1415,
       nameProduct: 'Lollipops',
       price: 9,
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -48,15 +51,17 @@ export class AppComponent {
       img: 'https://m.media-amazon.com/images/I/51zayXv469L._SL500_.jpg',
     },
     {
-      nameProduct: 'chewy rainbow',
-      price: 10,
+      _id: 1617,
+      nameProduct: 'Marshmallow',
+      price: 6.5,
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
-      img: 'https://angolodolce.net/cdn/shop/products/chewy_rainbow_candy_gommosi_zuccherati_fini_4_1024x1024.jpg?v=1607166950',
+      img: 'https://burst.shopifycdn.com/photos/marshmallow-candy-texture.jpg?exif=0&iptc=0',
     },
   ];
 
   productFiltered: {
+    _id: number;
     nameProduct: string;
     price: number;
     description: string;
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,4 +7,35 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
   @Input() dataParent: any;
+  idsSelected: number[] = [];
+  /*TO DO: 
+  1)Create a cart where you can place the selected products */
+
+  /*onAddProduct collects the IDs of the selected products:
+  1) inside filteredItem , the product with the ID 
+  corresponding to the value passed
+  as a parameter is filtered from the list of 
+  products (dataParent).  
+  2)if it actually returns an id from the list, 
+  it is inserted into the list of idsSelected. 
+  3)All products whose ids are present in this list will have
+  the borderCard class added*/
+  onAddProduct(id: number) {
+    //1)
+    const filteredItem = this.dataParent.filter(
+      (item: {
+        _id: number;
+        nameProduct: string;
+        price: number;
+        description: string;
+        img: string;
+      }) => {
+        return item._id === id;
+      }
+    );
+    //2)
+    if (filteredItem.length > 0) {
+      this.idsSelected.push(filteredItem[0]._id);
+    }
+  }
 }
